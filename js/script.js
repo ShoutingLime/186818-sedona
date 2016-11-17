436eddc4e053cc57cad62735459a3767c7e9c308
ShoutingLime <shouting.lime@gmail.com>
2016-11-17 12:23:56 +0300

    Commit 9
    Промежуточный коммит. Верстка со стилями. Нет эффектов при наведении и
    нажатии на элементы. Карта не интерактивна.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var link = document.querySelector(".search-form-button");
+var el = document.querySelector(".search-popup");
+
+link.addEventListener("click", function(event) {
+event.preventDefault();
+el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+});
