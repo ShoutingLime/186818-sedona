0006e15f9562daccd55d004dbfc14f13e93173a0
ShoutingLime <shouting.lime@gmail.com>
2016-11-23 16:12:20 +0300

    Commit 12
    Исправлены ошибки по базовым критериям. Анимирована форма. Подключены
    минифицированные файлы CSS и JS.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ var link = document.querySelector(".search-form-button");
 var el = document.querySelector(".search-popup");
 
 link.addEventListener("click", function(event) {
-event.preventDefault();
-el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+    event.preventDefault();
+    el.style.display = (el.style.display == 'none') ? 'block' : 'none';
 });
